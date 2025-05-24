1e645e625af707a1826453c7133fa86100377beb
suhzeet <sujeetneupane28@gmail.com>
2025-05-24 21:00:37 +0545

    updated

--- app/components/Achievements.jsx
+++ app/components/Achievements.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 function Achievements() {
   return (
-    <div id="achievements" className=" w-screen h-screen bg-[#323545]">
-      Achievements
+    <div
+      id="achievements"
+      className=" w-screen h-screen bg-[#323545] py-16 px-36"
+    >
+      <h2 className=" text-3xl text-center text-[#ffc257] font-semibold">
+        My Journey
+      </h2>
     </div>
   );
 }
