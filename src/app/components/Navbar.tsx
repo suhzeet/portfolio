deca1a4c7c1f614f479cd34fd437b6ea95ea79b4
suhzeet <sujeetneupane28@gmail.com>
2025-11-06 13:54:04 +0545

    first commit

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import {
+  Bars3Icon,
+  MoonIcon,
+  SunIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/solid";
+import { div } from "framer-motion/client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { useState } from "react";
+import { useTheme } from "../context/ThemeContext";
+
+const Navbar = () => {
+  const { theme, toggleTheme } = useTheme();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
+  const pathname = usePathname();
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const menuItems = [
+    { href: "/", label: "Home" },
+    { href: "/about", label: "About" },
+    { href: "/projects", label: "Projects" },
+    { href: "/blogs", label: "Blogs" },
+    { href: "/contact", label: "Contact" },
+  ];
+  return (
+    <nav className=" fixed w-full  backdrop-blur-sm z-50">
+      <div className="container max-w-7xl mx-auto px-4">
+        <div className="flex items-center justify-between h-16">
+          <Link href="/" className="text-4xl font-extrabold text-primary">
+            sujit.
+          </Link>
+
+          {/* desktop menu */}
+          <div className="hidden md:flex items-center space-x-8">
+            {menuItems.map((item) => {
+              const isActive = pathname === item.href;
+              return (
+                <Link
+                  key={item.href}
+                  href={item.href}
+                  className={`text-2xl font-semibold border-b-6 border-collapse rounded-sm hover:border-primary transition-color ${
+                    isActive ? " border-primary" : "border-transparent"
+                  }`}
+                >
+                  {item.label}
+                </Link>
+              );
+            })}
+            <button
+              onClick={toggleTheme}
+              className=" p-2 rounded-lg dark:text-white hover:bg-gray-100 hover:text-primary dark:hover:bg-gray-500 transition-colors cursor-pointer"
+            >
+              {theme === "dark" ? (
+                <SunIcon className="w-8 h-8" />
+              ) : (
+                <MoonIcon className="w-8 h-8" />
+              )}
+            </button>
+          </div>
+
+          {/* mobile menu botton */}
+          <button
+            className=" md:hidden p-2 rounded-lg hover:bg-gray-600 hover:text-primary dark:hover:bg-gray-800 transition-colors cursor-pointer"
+            onClick={toggleMobileMenu}
+          >
+            {isMobileMenuOpen ? (
+              <XMarkIcon className=" w-8 h-8" />
+            ) : (
+              <Bars3Icon className=" w-8 h-8" />
+            )}
+          </button>
+        </div>
+
+        {/* mobile menu */}
+        {isMobileMenuOpen && (
+          <div className="md:hidden">
+            <div className="py-4 space-y-4">
+              {menuItems.map((item, index) => (
+                <div key={index} onClick={toggleMobileMenu}>
+                  <Link
+                    href={item.href}
+                    className={` block py-2 text-xl font-semibold hover:text-primary transition-colors `}
+                  >
+                    {item.label}
+                  </Link>
+                </div>
+              ))}
+
+              <div>
+                <button
+                  onClick={toggleTheme}
+                  className=" flex text-xl font-semibold items-center py-2 hover:text-primary transition-colors"
+                >
+                  {theme === "dark" ? (
+                    <>
+                      <SunIcon className="w-6 h-6 mr-2" /> Light mode
+                    </>
+                  ) : (
+                    <>
+                      <MoonIcon className="w-6 h-6 mr-2" /> Dark mode
+                    </>
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
